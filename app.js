784195f45754ff20d35fc515e5c15af19d781999
Daniel Wagener <daniel.j.wagener@gmail.com>
2023-05-08 09:22:04 -0400

    Refactor into separate shop and admin routes

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/add-product", (req, res, next) => {
-	res.send("<h1>Add Product</h1>");
-});
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use("/", (req, res, next) => {
-	res.send("<h1>Hello from Express!</h1>");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+	res.status(404).send("<h1>Page not found</h1>");
 });
 
 app.listen(4000);
