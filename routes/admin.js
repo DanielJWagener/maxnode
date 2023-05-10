7c88c7a8cd51449d875c2af97e40c4099d1681ea
Daniel Wagener <daniel.j.wagener@gmail.com>
2023-05-10 10:38:00 -0400

    Statically serve CSS files

--- routes/admin.js
+++ routes/admin.js
@@ -5,11 +5,10 @@ const rootDir = require("../utils/path");
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-	console.log(rootDir);
 	res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
 	console.log(req.body);
 	res.redirect("/");
 });
