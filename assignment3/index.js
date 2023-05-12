d323197fdb51c1bb9973d28cfd0ac3a2ba86852c
Daniel Wagener <daniel.j.wagener@gmail.com>
2023-05-12 08:51:05 -0400

    Add routes

--- assignment3/index.js
+++ assignment3/index.js
@@ -3,3 +3,11 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
+
+const homeRoutes = require("./routes/home");
+const userRoutes = require("./routes/users");
+
+app.use(homeRoutes);
+app.use(userRoutes);
+
+app.listen(4000);
