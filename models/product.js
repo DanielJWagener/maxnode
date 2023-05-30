6a99799c0e056a4f99b091502124ed2978a5b691
Daniel Wagener <daniel.j.wagener@gmail.com>
2023-05-29 23:41:48 -0400

    Work with JSON

--- models/product.js
+++ models/product.js
@@ -1,4 +1,9 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+
+const rootPath = require("../utils/path");
+
+const pathToProductsFile = path.join(rootPath, "data", "products.json");
 
 module.exports = class Product {
 	constructor(title) {
@@ -6,10 +11,24 @@ module.exports = class Product {
 	}
 
 	save() {
-		products.push(this);
+		fs.readFile(pathToProductsFile, (err, fileContent) => {
+			let products = [];
+			if (!err) {
+				products = JSON.parse(fileContent);
+			}
+			products.push(this);
+			fs.writeFile(pathToProductsFile, JSON.stringify(products), (err) => {
+				console.log(err);
+			});
+		});
 	}
 
-	static fetchAll() {
-		return products;
+	static fetchAll(callback) {
+		fs.readFile(pathToProductsFile, (err, fileContent) => {
+			if (!err) {
+				callback([]);
+			}
+			callback(JSON.parse(fileContent));
+		});
 	}
 };
