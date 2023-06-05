378a308552bee03a37984c9e72fdfc558de3756d
Daniel Wagener <daniel.j.wagener@gmail.com>
2023-06-05 09:48:44 -0400

    Find product by id

--- models/product.js
+++ models/product.js
@@ -22,6 +22,7 @@ module.exports = class Product {
 	}
 
 	save() {
+		this.id = Math.random().toString();
 		getProductsFromFile((products) => {
 			products.push(this);
 			fs.writeFile(pathToProductsFile, JSON.stringify(products), (err) => {
@@ -32,5 +33,12 @@ module.exports = class Product {
 
 	static fetchAll(callback) {
 		getProductsFromFile(callback);
+	}
+
+	static findById(id, callback) {
+		getProductsFromFile((products) => {
+			const product = products.find((x) => x.id === id);
+			callback(product);
+		});
 	}
 };
