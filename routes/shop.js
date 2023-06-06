fd665e67b6eda0724dfad62a13454feee7a962ad
Daniel Wagener <daniel.j.wagener@gmail.com>
2023-06-06 09:31:57 -0400

    Add POST route for cart

--- routes/shop.js
+++ routes/shop.js
@@ -6,6 +6,7 @@ const shopController = require("../controllers/shop");
 router.get("/", shopController.getIndex);
 
 router.get("/cart", shopController.getCart);
+router.post("/cart", shopController.postCart);
 router.get("/products", shopController.getProducts);
 router.get("/products/:productId", shopController.getProduct);
 router.get("/orders", shopController.getOrders);
